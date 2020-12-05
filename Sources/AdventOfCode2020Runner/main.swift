7dfa24278f2c565f1ddd73c6f68c20f419a0e2aa
Jeff Kelley <SlaunchaMan@gmail.com>
2020-12-04 20:21:52 -0500

    Use swift-argument-parser

--- Sources/AdventOfCode2020Runner/main.swift
+++ Sources/AdventOfCode2020Runner/main.swift
@@ -6,20 +6,45 @@
 //
 
 import AdventOfCode2020
+import ArgumentParser
 
-for (index, puzzle) in AllPuzzles.enumerated() {
-    print("Day \(index + 1):")
+struct PuzzleSolver: ParsableCommand {
+    @Option(name: [.short, .customLong("day")])
+    var days: [Int] = []
     
-    if let puzzle = puzzle as? PuzzleWithExample1.Type {
-        print("\tExample 1: \(puzzle.example1())")
-    }
-
-    print("\tPart 1: \(puzzle.part1())")
-
-    if let puzzle = puzzle as? PuzzleWithExample2.Type {
-        print("\tExample 2: \(puzzle.example2())")
+    @Flag(name: .shortAndLong)
+    var verbose = false
+    
+    mutating func run() throws {
+        let puzzles: [Puzzle.Type]
+        
+        if days.isEmpty {
+            puzzles = AllPuzzles
+        }
+        else {
+            puzzles = AllPuzzles.filter {
+                days.contains($0.day)
+            } 
+        }
+        
+        puzzles.forEach { puzzle in
+            print("Day \(puzzle.day):")
+            
+            if let puzzle = puzzle as? PuzzleWithExample1.Type {
+                print("\tExample 1: \(puzzle.example1())")
+            }
+            
+            print("\tPart 1: \(puzzle.part1())")
+            
+            if let puzzle = puzzle as? PuzzleWithExample2.Type {
+                print("\tExample 2: \(puzzle.example2())")
+            }
+            
+            print("\tPart 2: \(puzzle.part2())")
+            print()
+        }
     }
     
-    print("\tPart 2: \(puzzle.part2())")
-    print()
 }
+
+PuzzleSolver.main()
