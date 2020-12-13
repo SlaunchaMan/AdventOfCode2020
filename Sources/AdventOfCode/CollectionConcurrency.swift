0f42513b97c95a8cab7059fc8b08bcb12a9c6641
Jeff Kelley <SlaunchaMan@gmail.com>
2020-12-13 17:06:42 -0500

    Even more concurrency.

--- Sources/AdventOfCode/CollectionConcurrency.swift
+++ Sources/AdventOfCode/CollectionConcurrency.swift
@@ -5,6 +5,7 @@
 //  Created by Jeff Kelley on 12/13/20.
 //
 
+import Algorithms
 import Foundation
 
 extension Sequence {
@@ -95,6 +96,84 @@ extension Sequence {
         )
 
         return group.wait(wallTimeout: timeout)
+    }
+
+}
+
+extension Sequence where Element: Comparable {
+
+    func leastConcurrent(
+        where predicate: @escaping (Element) -> Bool
+    ) -> Element? {
+        let minQueue = DispatchQueue(label: "FirstIndex",
+                                       attributes: .concurrent)
+
+        var minElement: Element?
+
+        forEachConcurrent { element in
+            if predicate(element) {
+                if let min = minQueue.sync(execute: { minElement }) {
+                    if min > element {
+                        minQueue.async(flags: .barrier) {
+                            minElement = element
+                        }
+                    }
+                }
+                else {
+                    minQueue.async(flags: .barrier) {
+                        minElement = element
+                    }
+                }
+
+                return true
+            }
+
+            return false
+        }
+
+        guard let minimum = minQueue.sync(execute: { minElement })
+        else { return nil }
+
+        return minimum
+    }
+
+}
+
+extension Collection {
+
+    func firstConcurrent(
+        where predicate: @escaping (Element) -> Bool
+    ) -> Element? {
+        let indexQueue = DispatchQueue(label: "FirstIndex",
+                                       attributes: .concurrent)
+
+        var firstIndex: Index?
+
+        indexed().forEachConcurrent { index, element in
+            if predicate(element) {
+                if let currentFirst = indexQueue.sync(execute: { firstIndex }) {
+                    if currentFirst > index {
+                        indexQueue.async(flags: .barrier) {
+                            firstIndex = index
+                        }
+                    }
+                }
+                else {
+                    indexQueue.async(flags: .barrier) {
+                        firstIndex = index
+                    }
+                }
+
+                return true
+            }
+
+            return false
+        }
+
+        guard let index = indexQueue.sync(execute: { firstIndex })
+        else { return nil }
+
+        return self[index]
     }
 
 }
