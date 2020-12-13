c7b62f8bd8b8dfb20c65bd41fa4513d6c9402261
Jeff Kelley <SlaunchaMan@gmail.com>
2020-12-13 12:58:39 -0500

    Add 2020 Day 13.

---
+++ Sources/AdventOfCode/2020/2020Day13.swift
@@ -0,0 +1,127 @@
+//
+//  2020Day13.swift
+//  AdventOfCode
+//
+//  Created by Jeff Kelley on 12/13/20.
+//
+
+import Algorithms
+import Foundation
+
+private let exampleInput =
+    """
+    939
+    7,13,x,x,59,x,31,19
+    """
+
+extension Year2020 {
+
+    public enum Day13: Puzzle {
+
+        public static let year: Year.Type = Year2020.self
+
+        public static let day = 13
+
+        private static func nextBus(
+            in busIDs: [Int],
+            after startTime: Int
+        ) -> (busID: Int, departureTime: Int) {
+            var departureTimes = [Int](repeating: 0, count: busIDs.count)
+
+            while departureTimes.contains(where: { $0 <= startTime }) {
+                for (index, busID) in busIDs.indexed()
+                where departureTimes[index] < startTime {
+                    departureTimes[index] += busID
+                }
+            }
+
+            let smallestMultiple = departureTimes.min()!
+
+            let index = departureTimes.firstIndex(of: smallestMultiple)!
+
+            return (busIDs[index], departureTimes[index])
+        }
+
+        public static func example1() -> String {
+            let lines = parseInputLines(exampleInput)
+            let startTime = Int(lines[0])!
+
+            let busIDs = lines[1]
+                .components(separatedBy: ",")
+                .compactMap(Int.init)
+
+            let (busID, departureTime) = nextBus(in: busIDs, after: startTime)
+
+            return "\((departureTime - startTime) * busID)"
+        }
+
+        public static func part1() -> String {
+            let lines = parseInputLines(puzzleInput())
+            let startTime = Int(lines[0])!
+
+            let busIDs = lines[1]
+                .components(separatedBy: ",")
+                .compactMap(Int.init)
+
+            let (busID, departureTime) = nextBus(in: busIDs, after: startTime)
+
+            return "\((departureTime - startTime) * busID)"
+        }
+
+        private static func firstSubsequentDepartureSequence(
+            busIDs: [Int?]
+        ) -> Int {
+            let offsetsAndIDs: [(index: Int, id: Int)] = busIDs.indexed()
+                .compactMap { (index, id) -> (Int, Int)? in
+                    guard let id = id else { return nil }
+                    return (index, id)
+                }
+
+            let first = offsetsAndIDs[0]
+
+            var elements: [(index: Int, id: Int)] = [first]
+
+            var step = first.id
+
+            for (index, id) in offsetsAndIDs.dropFirst() {
+                let stride = Swift.stride(
+                    from: step,
+                    through: .max,
+                    by: elements.map { $0.1 }.reduce(1, *)
+                )
+
+                elements.append((index, id))
+
+                step = stride.first { s in
+                    elements.allSatisfy { element in
+                        (s + element.index).isMultiple(of: element.id)
+                    }
+                }!
+            }
+
+            return step
+        }
+
+        public static func example2() -> String {
+            let lines = parseInputLines(exampleInput)
+
+            let busIDs = lines[1]
+                .components(separatedBy: ",")
+                .map(Int.init)
+
+            return "\(firstSubsequentDepartureSequence(busIDs: busIDs))"
+        }
+
+        public static func part2() -> String {
+            let lines = parseInputLines(puzzleInput())
+
+            let busIDs = lines[1]
+                .components(separatedBy: ",")
+                .map(Int.init)
+
+            return "\(firstSubsequentDepartureSequence(busIDs: busIDs))"
+        }
+
+    }
+
+}
---
+++ Tests/AdventOfCodeTests/2020/2020Day13Tests.swift
@@ -0,0 +1,29 @@
+//
+//  2020Day13Tests.swift
+//  AdventOfCodeTests
+//
+//  Created by Jeff Kelley on 12/13/20.
+//
+
+import AdventOfCode
+import XCTest
+
+class Year2020Day13Tests: XCTestCase {
+
+    func testExample1() {
+        XCTAssertEqual(Year2020.Day13.example1(), "295")
+    }
+
+    func testPart1() {
+        XCTAssertEqual(Year2020.Day13.part1(), "246")
+    }
+
+    func testExample2() {
+        XCTAssertEqual(Year2020.Day13.example2(), "1068781")
+    }
+
+    func testPart2() {
+        XCTAssertEqual(Year2020.Day13.part2(), "939490236001473")
+    }
+
+}
